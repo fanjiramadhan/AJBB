36313e37b699625ee7dc75b294c7057a1d9dcbdf
Fanji Ramdhan <faanji.ramadhan.199@gmail.com>
2020-09-27 09:39:41 +0700

    anyar

---
+++ application/views/index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bumiputera</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url();?>assets/css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.<?php echo base_url();?>assets/js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">Sistem Informasi Lokasi Kantor AJB Bumiputera 1912 Di Kota Bandung</div>
+    <div class="address-bar">Proven Overtime</div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">AJB Bumiputera</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="<?php echo base_url()?>index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url()?>index.php/destinasi">Lokasi</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url()?>index.php/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12 text-center">
+                    <div id="carousel-example-generic" class="carousel slide">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="img-responsive img-full" src="<?php echo base_url();?>assets/img/11.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="<?php echo base_url();?>assets/img/22.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="<?php echo base_url();?>assets/img/33.jpg" alt="">
+                            </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="" lass="icon-prev"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="icon-next"></span>
+                        </a>
+                    </div>
+                    <h2 class="brand-before">
+                        <small>Welcome to</small>
+                    </h2>
+                    <h1 class="brand-name">Bumiputera</h1>
+                    <hr class="tagline-divider">
+                    <h2>
+                        <small>
+                            <strong>1912</strong>
+                        </small>
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Tentang
+                        <strong>Bumiputera</strong>
+                    </h2>
+                    <hr>
+                    <img class="img-responsive img-border img-left" src="<?php echo base_url();?>assets/img/bumi.jpg" alt="">
+                    <hr class="visible-xs">
+                    <p>AJB Bumiputera 1912 merupakan satu-satunya perusahaan asuransi jiwa di Indonesia yang berbentuk badan hukum usaha bersama/mutual. Usaha bersama Asuransi ini berdiri sejak 1912 dan berkantor pusat di Jakarta.</p>
+                </div>
+            </div>
+        </div>
+
+<div class="row">
+
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">prodak
+                        <strong>asuransi</strong>
+                    </h2>
+                    <hr>
+                    <div class="col-lg-12">
+
+                        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Mitra
+                        <strong>beasiswa</strong>
+                    </h2>
+                    <hr>
+                    <img class="img-responsive img-border img-left" src="<?php echo base_url();?>assets/img/bumi.jpg" alt="">
+                    <hr class="visible-xs">
+                    <p>Setiap orang tua menginginkan masa depan yang cerah bagi anak-anak mereka, dan memberikan mereka awal yang lebih baik. 
+                        Namun banyak orang tua kuatir bahwa melonjaknya biaya sekolah akan menempatkan pendidikan yang baik di luar jangkauan anak-anak mereka, dan kuatir akan masa depan anak-anaknya jika terjadi sesuatu terhadap mereka.
+                    
+Mitra Beasiswa disediakan dalam mata uang Rupiah dan merupakan program Mitra Beasiswa yang menjamin pembiayaan pendidikan 
+anak sepenuhnya, mulai dari taman kanak-kanak hingga perguruan tinggi, terlepas dari perubahan keadaan keuangan.
+Mitra Beasiswa dirancang khusus untuk menjadi mitra anak dalam pendidikan, memastikan anak-anak Anda secara teratur
+ mendapatkan uang yang mereka butuhkan untuk melanjutkan pendidikan mereka. Masa depan anak-anak juga terlidungi karena program ini dirancang untuk memastikan agar mereka tetap mendapatkan dana beasiswa hingga mereka lulus, walaupun jika orang tua mereka meninggal dunia.
+Beragam Manfaat
+Melalui Mitra Beasiswa, manfaat yang Anda akan dapatkan meliputi:
+
+<P>a Dana Kelangsungan Belajar (DKB) yang dibayarkan secara bertahap, sesuai dengan tingkat usia anak, baik Tertanggung hidup atau meninggal dunia.
+<p>b. Dana Beasiswa anak, dibayarkan pada saat periode asuransi berakhir, baik tertanggung masih hidup atau meninggal dunia.
+<p>c. Santunan meninggal dunia sebesar 100% dari uang pertanggungan.
+<p>d. Bebas premi bagi polis jika Tertanggung meninggal dunia.
+<p>e. Pengembalian simpanan premi bagi polis saat Tertanggung meninggal dunia jia premi dibayarkan secara penuh setelah jumlah premi diperhitungkan
+<p>f. Hak untuk mendapatkan Reversionary Bonus, jika Tertanggung meninggal dunia, penebusan polis, atau habis kontrak.
+
+<P>Persyratan
+Jika Anda berusia 21 tahun atau sudah menikah, Anda berhak untuk menjadi seorang pemegang polis. Pemegang polis belum tentu termasuk Tertanggung, tetapi antara Pemegang Polis dan Tertanggung, hubungan kepentingan dalam asuransi - suami, istri dan anak yang sah.
+Masa pertanggungan untuk asuransi ini adalah minimum 2 tahun dan maksimum 17 tahun. Premi dibayar dalam mata uang Rupiah, dan dapat dibayar sekaligus, tahunan, setengah tahunanan, atau tiap tiga bulan.
+Asuransi dapat diperoleh dengan atau tanpa pemeriksaan kesehatan. Masa observasi untuk kewajiban tanpa pemeriksaan kesehatan adalah 2 tahun, kecuali jika terjadi kecelakaan atau adanya wabah penyakit yang telah dinyatakan oleh lembaga kesehatan setempat, di mana ketentuan untuk masa observasi tanpa pemeriksaan kesehatan tidak berlaku.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Mitra
+                        <strong>Mandiri</strong>
+                    </h2>
+                    <hr>
+                    <img class="img-responsive img-border img-left" src="<?php echo base_url();?>assets/img/bumi.jpg" alt="">
+                    <hr class="visible-xs">
+                    <p>Ciptakan Kesejahteraan Secara Mandiri</p>
+
+<P>orang mendambakan kesejahteraan untuk dirinya dan keluarga yang dicintainya. Namun, keterbatasan penghasilan menjadi kendala untuk mewujudkan kesejahteraan yang diidamkan. Bahkan, tak jarang kesejahteraan menjadi sesuatu yang jauh dari jangkauan. Salah satu cara mewujudkan kesejahteraan tanpa harus menunggu penghasilan lebih adalah program Mitra (Bumiputera) Proteksi Mandiri.
+Mitra Proteksi Mandiri menawarkan solusi bagi Anda yang ingin menciptakan sendiri kesejahteraan keluarga tanpa mengharapkan kontribusi pihak lain.
+Dengan memiliki Mitra Proteksi Mandiri, Anda telah melakukan langkah awal yang tepat untuk mendapatkan kepastian kesejahteraan keluarga yang akan terus meningkat dari tahun ke tahun, sekaligus mendapatkan manfaat tabungan.</P>
+<P>Mitra Proteksi Mandiri diperuntukkan bagi Anda untuk merencanakan kesejahteraan masa pensiun. Mitra Proteksi Mandiri adalah program asuransi mikro dari Bumiputera untuk Anda yang berprofesi sebagai petani, nelayan, peternak atau pekerja lainnya. Meskipun berpenghasilan terbatas, Anda tetap bisa menikmati masa pensiun dengan tenang layaknya seorang pegawai.
+Manfaat </P>
+<p>1. Jika Tertanggung hidup hingga akhir masa asuransi, maka pemegang polis akan menerima Nilai Tunai pada saat habis kontrak.
+
+<p>2. Apabila Tertanggung meninggal dunia dalam masa asuransi, maka ahli waris sebagai pihak yang ditunjuk akan menerima santunan meninggal sebesar :
+• Uang Pertanggungan yang meningkat pada saat meninggal
+• Nilai tunai pada saat meninggal   
+Selanjutnya asuransi berakhir.</p>
+<P>3. Jika Pemegang Polis mengundurkan diri, maka ia akan menerima Nilai Tunai pada saat mengundurkan diri dan selanjutnya asuransi berakhir.
+
+Keunggulan
+
+• Bebas biaya polis
+• Premi minimal hanya Rp.200.000,- per bulan / polis
+• Uang Pertanggungan pada awal asuransi sebesar 24 kali premi bulanan.
+• Uang Pertanggungan Mitra Proteksi Mandiri terus meningkat sebesar 10% dari Uang Pertanggungan awal setiap tahun mulai tahun ke dua dan seterusnya hingga akhir kontrak atau jika tertanggung meninggal dunia.</p>
+
+<P>Anda berminat ?
+
+Mitra Proteksi Mandiri bisa dimiliki bagi Anda yang berusia minimal 15 tahun dan maksimal 55 tahun.*
+
+<P>*Syarat dan Ketentuan Berlaku!.</p>
+                </div>
+            </div>
+        </div>
+
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Contact
+                        <strong>form</strong>
+                    </h2>
+                    <hr>
+
+                                    <?php
+                                    
+                                          if(isset($komen)){
+                                                foreach($komen as $row){
+                                            ?>
+                                        <tr>
+                                            <strong><?php echo $row->nama; ?></strong><br>
+                                            <?php echo $row->email; ?><br>
+                                            <?php echo $row->ket; ?><br>
+                                        <br></br>
+                                        </tr>
+                                    <?php }
+                                             }
+                                                ?>
+                    <br></br>                              
+                    <form role="form" action="<?php echo base_url()?>index.php/index/tambah" method="POST">
+                        <div class="row">
+                            <div class="form-group col-lg-6">
+                                <label>Name</label>
+                                <input name="nama" type="text" class="form-control">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <label>Email Address</label>
+                                <input name="email" type="email" class="form-control">
+                            </div>
+                            <div class="clearfix"></div>
+                            <div class="form-group col-lg-12">
+                                <label>Message</label>
+                                <textarea name="ket" class="form-control" rows="6"></textarea>
+                            </div>
+                            <div class="form-group col-lg-12">
+                                <input type="hidden" name="save" value="contact">
+                                <button type="submit" class="btn btn-default">send</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                   <?php
+            $sql ="SELECT * FROM contact_form";
+            $query = $this->db->query($sql);
+            if ($query->num_rows() > 0) {
+              foreach ($query->result() as $row) {?>
+                <p><?php echo $row->nama;?></p>
+                <p><?php echo $row->email;?></p>
+                <p><?php echo $row->ket;?></p>
+                <br>
+            <?php
+                $sql2="select * from balas where id_cf='".$row->id_cf."'";
+                $query2=$this->db->query($sql2);
+                foreach ($query2->result() as $row2) {?>
+                <div style="color:blue;margin-left:30px;">
+                <p>Admin</p>
+                <p><?php echo $row2->komentarb;?></p>
+                </div>
+                <br>
+
+
+           <?php
+             }
+            }
+        }
+
+            ?>
+
+            </div>
+         
+        </div>
+    <!-- /.container -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Fanji Ramadhan 2020</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="<?php echo base_url();?>assets/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
